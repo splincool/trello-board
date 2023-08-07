39e7c04e9bf96e4c0c4f35c61883b49a81142b21
Maksim Ivanov <mak.ivanov@gmail.com>
2023-08-07 23:33:32 +0300

    init commit to add project

---
+++ components/TrelloBoard.vue
@@ -0,0 +1,99 @@
+<script setup lang="ts">
+import { Column, Task } from '@/types';
+import { nanoid } from 'nanoid';
+import draggable from 'vuedraggable';
+
+const columns = ref<Column[]>([
+    {
+        id: nanoid(),
+        title: "Backlog",
+        tasks: [
+            {
+                id: nanoid(),
+                title: "Create marketing landing page",
+                createdAt: new Date(),
+            },
+            {
+                id: nanoid(),
+                title: "Develop cool new feature",
+                createdAt: new Date(),
+            },
+            {
+                id: nanoid(),
+                title: "Develop a weather widget",
+                createdAt: new Date(),
+            }
+        ],
+    },
+    {
+        id: nanoid(),
+        title: 'Selected for Dev',
+        tasks: [
+            {
+                id: nanoid(),
+                title: 'Create a branch',
+                createdAt: new Date(),
+            },
+            {
+                id: nanoid(),
+                title: 'Change width of left menu',
+                createdAt: new Date(),
+            }
+        ],
+    },
+    {
+        id: nanoid(),
+        title: 'In Progress',
+        tasks: [],
+    },
+    {
+        id: nanoid(),
+        title: 'QA',
+        tasks: [],
+    },
+    {
+        id: nanoid(),
+        title: 'Complete',
+        tasks: [],
+    }
+]);
+
+const alt = useKeyModifier("Alt");
+</script>
+
+<template>
+    <div>
+        <draggable
+            v-model="columns"
+            group="columns"
+            item-key="id"
+            animation="150"
+            handle=".drag-handle"
+            class="flex gap-4 overflow-x-auto items-start"
+        >
+            <template #item="{ element: column }: { element: Column }">
+                <div class="column bg-gray-200 p-5 rounded min-w-[250px]">
+                    <header class="font-bold mb-4">
+                        <DragHandle />
+                        {{ column.title }}
+                    </header>
+                    <draggable
+                        v-model="column.tasks"
+                        :group="{ name: 'tasks', pull: alt ? 'clone' : true }"
+                        item-key="id"
+                        handle=".drag-handle"
+                    >
+                        <template #item="{ element: task } : {element: Task}">
+                            <div>
+                                <TrelloBoardTask :task="task" />
+                            </div>
+                        </template>
+                    </draggable>
+                    <footer>
+                        <button class="text-gray-500">+ Add a card</button>
+                    </footer>
+                </div>
+            </template>
+        </draggable>
+    </div>
+</template>
---
+++ components/TrelloBoardTask.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import type { Task } from '@/types';
+
+defineProps<{
+    task: Task;
+}>()
+</script>
+
+<template>
+    <div 
+        :title="task.createdAt.toLocaleString()"
+        class="task bg-white p-2 mb-2 rounded shadow-sm max-w-[250px] flex"
+    >
+        <DragHandle class="pr-2"/>
+        <span>
+            {{ task.title }}
+        </span> 
+    </div>
+</template>
+
+<style>
+    .sortable-drag .task {
+        transform: rotate(5deg);
+    }
+
+    .sortable-ghost .task {
+        position: relative;
+    }
+
+    .sortable-ghost .task::after {
+        content: "";
+        @apply absolute top-0 bottom-0 left-0 right-0 bg-slate-300 rounded;
+    }
+</style>
